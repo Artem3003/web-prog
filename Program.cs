945ae330c86d74c5e5b23b6bd247775f597e5442
Artem <temapro04@gmail.com>
2025-03-26 15:23:57 +0200

    Settings of server added.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseKestrel();
+
+builder.Services.AddControllers();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 app.MapGet("/student", () => "Artem Yurchenko [KP-22]");
+
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
